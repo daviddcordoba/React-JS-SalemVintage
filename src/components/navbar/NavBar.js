10999b5950d68fda2ea209e4f4238151a6579e0f
David <cordoba_d@rocketmail.com>
2022-07-14 18:09:54 -0300

    primercommit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+import '../navbar/NavBar.css'
+
+const NavBar = () => {
+
+    return (
+        <div>
+            <h1 className='soyunlogo'>SalemVintage</h1>
+        </div>
+    )
+
+}
+
+export default NavBar
