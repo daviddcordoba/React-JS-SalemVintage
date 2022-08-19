7bc7139a9c74080728bc50f1ff03d2290dfdf987
David <cordoba_d@rocketmail.com>
2022-08-19 19:43:35 -0300

    commit solucionadoCategorias

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,6 +3,11 @@ import CartWidget from './CartWidget';
 import {Link} from 'react-router-dom'
 import { useContext } from 'react';
 import { CartContext } from '../Context/CartContext';
+import { Dropdown } from 'react-bootstrap';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -14,7 +19,7 @@ function NavBar() {
     <div className='navBar'>
       <nav>
       <Link to='/'>
-        <h2>Salem <span>Vintage</span></h2>
+        <h2><span>Salem Vintage</span></h2>
         </Link>
         <ul>
           <Link to='/'>
@@ -29,7 +34,21 @@ function NavBar() {
           </li>
           </Link>
 
-          <Link to='/productos'>
+          <li >
+              <Dropdown>
+                <Dropdown.Toggle className='' variant="" id="dropdown-basic">
+                  Productos
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/categoria/camisas">Camisas</Dropdown.Item>
+                  <Dropdown.Item href="/categoria/pantalones">Pantalones</Dropdown.Item>
+                  <Dropdown.Item href="/productos">Ver todo</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>          
+          </li>  
+
+          {/* <Link to='/productos'>
           <li >
             Productos
           </li>
@@ -43,7 +62,7 @@ function NavBar() {
           <li >
             Pantalones
           </li>
-          </Link>
+          </Link> */}
           <Link to='/contacto'>
           <li >
             Contacto
