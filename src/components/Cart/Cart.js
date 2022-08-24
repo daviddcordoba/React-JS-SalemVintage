18f6157eb72742b39e00db0a247012d27d4745bd
David <cordoba_d@rocketmail.com>
2022-08-24 00:07:18 -0300

    commit firebase 2

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,56 @@
 import CartList from "./CartList"
 import './Cart.scss'
 import DeleteIcon from '@mui/icons-material/Delete';
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../Context/CartContext"
 import { Link } from "react-router-dom";
+import { addDoc, collection, getFirestore} from 'firebase/firestore'
 
 const Cart = () => {
     const {clear,cart} = useContext(CartContext)
-    
 
+    const order ={
+        buyer:{
+            name: '',
+            email:'',
+            phone:'',
+            adress:''
+        },
+        items: cart.map(product => ({
+            id : product.id,
+            title: product.title,
+            price: product.price,
+            quantity: product.quantity
+        })),
+        total:cart.reduce((acc,prod) => acc + (prod.price*prod.quantity), 0) ,
+    }
+
+    const [showForm, setShowForm] = useState(false)
+    const orderGenerate = () =>{
+        /* Intancia de firestore y referencia */
+        const db = getFirestore()
+        const ordersCollection = collection(db,'orders')
+
+        /* Promesa para que guarde order en ordersCollection*/
+
+        addDoc(ordersCollection,order)
+                                        .then( ({id}) => console.log(id))
+
+        
+        setTimeout(() => {
+            setShowForm(true)
+        }, 2000);
+    }
+    
+    const handlechange = (e) => {
+        order.buyer.e.target.value
+    }
+    
     return(
         <div className="container cartContainer">
             {
                 cart.length === 0 ? <Link to='/productos'><h2>No hay items, volver a comprar</h2> </Link>
+                                    
                                     :(<table>
                                         <thead>
                                             <tr>
@@ -24,15 +62,40 @@ const Cart = () => {
                                         </thead>
                                         <tbody >
                                             <CartList/>
-                                        </tbody>
-                        
+                                        </tbody>                        
                                         <tfoot>
                                             <tr>
                                                 <th>Total : ${cart.reduce((acc,prod) => acc + (prod.price*prod.quantity), 0)} </th>
+                                                <th><button onClick={orderGenerate}>Generar orden</button></th>
                                             </tr>
                                         </tfoot>
                                     </table>)
             }
+            
+            {showForm && (<form>
+                            <input 
+                            type='text' 
+                            name='name' 
+                            placeholder= "Ingresa tu nombre"
+                            value={order.buyer.name}
+                            onChange = {handlechange}
+                            />
+                            <input 
+                            type='number' 
+                            name='phone'
+                            placeholder="Ingresa tu telefono"
+                            value={order.buyer.phone} 
+                            />
+                            <input 
+                            type='email' 
+                            name='email' 
+                            placeholder="Ingresa tu email"
+                            value={order.buyer.email} 
+                            />
+                            
+                            
+                            <button>Enviar</button>
+                        </form>) }
             
         </div>
     )
